59bb28add5a8d139d1938c628b703a444fbf2c1f
seulbi <sosb0421@gmail.com>
2023-04-26 15:16:40 +0900

    add config_db.json

--- flask/apps/models/chat/basic.py
+++ flask/apps/models/chat/basic.py
@@ -2,6 +2,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
 from config import ModelConfig
+import jsonpickle
 
 class SimpleChat:
     '''
@@ -11,13 +12,24 @@ class SimpleChat:
     def __init__(self, prompt):
         self.llm = ChatOpenAI(openai_api_key = ModelConfig.GPT.API_KEY,temperature=0.0)
         self.prompt = prompt
+<<<<<<< Updated upstream
+=======
+        self.chatgpt_chain = None
+        
+>>>>>>> Stashed changes
         self.chatgpt_chain = ConversationChain(
             llm = self.llm, 
             prompt = self.prompt,
             verbose = True, 
+<<<<<<< Updated upstream
             memory = ConversationBufferMemory(return_messages=True)
         )
 
+=======
+            #memory= self.memory
+            memory = ConversationBufferMemory(return_messages=True)
+        )
+>>>>>>> Stashed changes
 
     def chain(self, input):
         '''
@@ -27,8 +39,14 @@ class SimpleChat:
             - output(string): GPT 모델의 답변
         '''
 
+<<<<<<< Updated upstream
+=======
+    
+       
+>>>>>>> Stashed changes
         output = self.chatgpt_chain.predict(input=input)
 
         return output
 
-
+    def to_json(self):
+        return jsonpickle.encode(self)
