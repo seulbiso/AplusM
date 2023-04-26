0a14c3726d272b0912bddcb020d390d4a9f182f0
ys <kkys96@gmail.com>
2023-04-26 16:08:15 +0900

    front_0426

--- flask/apps/controllers/index/controllers.py
+++ flask/apps/controllers/index/controllers.py
@@ -1,10 +1,30 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, Flask
 
 from apps.common.response import ok
 from config import Config
 
-app = Blueprint('index', __name__, url_prefix='/')
+app = Blueprint('dist', __name__, url_prefix='/')
 
-@app.route('', methods=['GET'])
+@app.route('', methods=['POST', 'GET'])
 def index():
-    return render_template('layout/index.html')
+    return render_template('dist/index.html')
+
+
+
+@app.route('/chat', methods=['POST', 'GET'])
+def chat():
+    if request.method == 'POST':
+        temp = request.form['chatMessage']
+    elif request.method == 'GET':
+        pass
+    return render_template('dist/index.html', chatMessage=temp)
+
+
+
+
+
+
+
+
+
+
