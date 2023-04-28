cb98be433bf3b1e22f6c779f9f1633a920786390
yumin <rhawl9764@naver.com>
2023-04-28 09:41:38 +0900

    clone controllers

--- flask/apps/controllers/index/controllers.py
+++ flask/apps/controllers/index/controllers.py
@@ -1,13 +1,13 @@
-from flask import Blueprint, render_template, session, url_for, redirect, request, g , jsonify
+from flask import Blueprint, render_template, session, url_for, redirect, request, g, jsonify
 import random
-import jsonpickle
+import jsonpickle, json
 
 
 from apps.common.response import ok
 from config import Config
 
 from apps.models.prompt.preprocess import Prompt
-from apps.models.chat.basic import SimpleChat
+from apps.models.chat.chain import SimpleChat
 
 from apps.database.models import history_head
 
@@ -16,19 +16,22 @@ app = Blueprint('dist', __name__, url_prefix='/')
 
 @app.route('', methods=['GET'])
 def index():
-    #test_connect_db()
+    # test_connect_db()
     session.clear()
     if 'session_number' not in session:
         set_session()
-    
+
     return render_template('dist/index.html')
+
 
 def test_connect_db():
     rows = history_head.query.all()
     print("rows ", rows)
 
+
 def set_session():
-    session['session_number'] = random.randrange(1,100)
+    session['session_number'] = random.randrange(1, 100)
+
 
 @app.route('clear', methods=['GET'])
 def clear():
@@ -36,42 +39,55 @@ def clear():
     return redirect(url_for('.index'))
 
 
-@app.route('chat', methods= ['POST'])
+@app.route('chat', methods=['POST'])
 def chat():
 
     # Persona(dict)
-    persona = {"persona_age":"60", "persona_personality":"따뜻한 엄마같은"}
+    # persona = {"persona_age": "60", "persona_personality": "따뜻한 엄마같은"}
 
     # User Info(dict)
-    user_info = {"user_info_age":"24", "user_info_job":"취업준비생"}
-
-
-    # Check if SimpleChat instance exists in session
-    if 'simple_chat' not in session:
-        # Create a new SimpleChat instance and store it in session
-        prompt = Prompt().write_prompt(persona, user_info)
-        simple_chat = SimpleChat(prompt=prompt)
-        session['simple_chat'] = simple_chat.to_json()
-
-    else:
-        # Retrieve SimpleChat instance from session
-        simple_chat = jsonpickle.decode(session['simple_chat'])
+    # user_info = {"user_info_age": "24", "user_info_job": "취업준비생"}
     
-
     if request.method == 'POST':
+
         # Get user input from form data
-        input = request.form['chatMessage']
-        output = simple_chat.chain(input)
+        input = request.form['chat_Q']
+        persona = request.form['persona']
+        #persona = "친절한 상담원"
+        user_info = json.loads(request.form['user_info'])
         
+        print('logssssssssssssss')
+        print(user_info)
+        print(type(user_info))
+
+        # for k in user_info:
+        #     print(type(user_info[k]))
+        #     print(k)
+        #     print(v)
+
+        # Check if SimpleChat instance exists in session
+        if 'simple_chat' not in session:
+            # Create a new SimpleChat instance and store it in session
+            prompt = Prompt().write_prompt(persona, user_info)
+            simple_chat = SimpleChat(prompt=prompt)
+            session['simple_chat'] = simple_chat.to_json()
+
+        else:
+            # Retrieve SimpleChat instance from session
+            simple_chat = jsonpickle.decode(session['simple_chat'])     
+
+        output = simple_chat.chain(input)   
+    
+        #Logging
+        #app.logger.info("-------------request data---------------------------")
         #print output
         print(output)
+        print(persona)
+        print(user_info)
         session['simple_chat'] = simple_chat.to_json()
-    
+
         response= {
-            'chatMessage' : output
+            'chat_A' : output
         }
 
     return jsonify(response)
-
-
-
